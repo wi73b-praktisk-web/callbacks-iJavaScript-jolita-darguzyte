4644da037475b52a0bf667046e455df88d670533
jolitadarguzyte <4364891@rts-365.dk>
2017-08-27 20:38:46 +0200

    first commen

---
+++ server.js
@@ -0,0 +1,22 @@
+
+
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const router = require('./controllers/routesController');
+
+http.createServer(function (req, res) {
+    router.init(req, res);
+}).listen(port, hostname);
+
+console.log(`serveren kører på http://${hostname}:${port}/`);
+
+
+
+
+
+
+
+    
